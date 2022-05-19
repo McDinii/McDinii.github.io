cd526d55173ab033364f6e828c782a7cf1d55e76
McDinii <denisnechay@gmail.com>
2022-05-19 08:25:08 +0300

    end PZ

---
+++ js/scroll.js
@@ -0,0 +1,27 @@
+let Links = document.querySelectorAll( '[data-goto]' );
+console.log( Links )
+if (Links.length > 0) {
+    Links.forEach( Link => {
+        Link.addEventListener( "click", onMenuLinkClick );
+    } );
+
+    function onMenuLinkClick(e) {
+        const Link = e.target;
+        const gotoBlock = document.querySelector( Link.dataset.goto );
+        const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+
+        header_burger = document.querySelector( '.header_burger' );
+        header_menu = document.querySelector( '.header_menu' );
+        if (header_burger.classList.contains( 'active' )) {
+            document.body.classList.remove( 'lock' );
+            header_burger.classList.remove( 'active' );
+            header_menu.classList.remove( 'active' );
+        }
+
+        window.scrollTo( {
+            top: gotoBlockValue,
+            behavior: "smooth"
+        } );
+    };
+}
+;
